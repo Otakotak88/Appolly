001864f362087b2060eed8c813ae43b86bb32549
Otavio <otaviocds03@gmail.com>
2025-07-15 22:10:12 -0300

    Better visibility; Some bugs fixed

--- hamburger-bar.js
+++ hamburger-bar.js
@@ -4,3 +4,7 @@ const hamburgerBar = document.querySelector(".menu-hamburger");
 hamburgerBar.addEventListener("click", () => {
     navbar.classList.toggle("links-active")
 })
+
+addEventListener("resize", () => {
+    navbar.classList.remove("links-active")
+})
